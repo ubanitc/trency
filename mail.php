c87fcee5a40087c3c765059bd031f9db6e086404
ubanitc <57474291+ubanitc@users.noreply.github.com>
2020-05-05 00:51:33 -0700

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,32 @@
+<?php
+
+    require("./mail/phpmailer/PHPMailerAutoload.php");
+
+            $mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'stmp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'tochukwu.ubani@gmail.com';                 // SMTP username
+$mail->Password = 'Exhibit911@';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('tochukwu.ubani@gmail.com', 'Mailer');
+$mail->addAddress('tochukwu.ubani@yahoo.com');     // Add a recipient
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+?>
